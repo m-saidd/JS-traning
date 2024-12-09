440957789d657b69dc843466e952ce646be2e63d
Mohamed Said <160847073+m-saidd@users.noreply.github.com>
2024-12-10 01:01:13 +0200

    Add files via upload

---
+++ To-Do List/js/main.js
@@ -0,0 +1,36 @@
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+
+function addTask() {
+    if(input.value === "") {
+        alert("please, enter your task...");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "x";
+        li.appendChild(span);
+    }
+    input.value = "";
+    saveTask();
+}
+
+list.addEventListener("click", function(element) {
+    if(element.target.tagName === "LI") {
+        element.target.classList.toggle("select");
+    } else if(element.target.tagName === "SPAN") {
+        element.target.parentElement.remove();
+    }
+    saveTask();
+}, false);
+
+function saveTask() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showTask();
